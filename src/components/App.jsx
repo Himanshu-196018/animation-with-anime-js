8a9e3734f7ed13aec8ce31a1b0e745b1ab224d54
Himanshu <himanshu_196018@saitm.org>
2023-10-21 20:50:11 +0530

    animatin part 5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import anime from "animejs/lib/anime.es.js";
 import { useRef, useEffect } from "react";
 import Box from "./Box";
+import Matrix from "./Matrix";
 import ObjectDisplay from "./ObjectDisplay";
 
 const App = () => {
@@ -15,143 +16,125 @@ const App = () => {
 
     // adding animations
     // targetting specific class
-    animation.current.add(
-      anime({
-        targets: `.wrap .box-1`,
-        translateX: 250,
-        loop: true,
-      })
-    );
+    anime({
+      targets: `.wrap .box-1`,
+      translateX: 250,
+      loop: true,
+    });
 
     // targetting specific node
-    animation.current.add(
-      anime({
-        targets: document.querySelectorAll(".box-2"),
-        translateX: 270,
-        loop: true,
-      })
-    );
+    anime({
+      targets: document.querySelectorAll(".box-2"),
+      translateX: 270,
+      loop: true,
+    });
 
     // targetting JS object
-    animation.current.add(
-      anime({
-        targets: battery,
-        charged: "100%",
-        cycles: 130,
-        round: 1,
-        loop: true,
-        update: function () {
-          document.querySelector(".battery-log").innerHTML =
-            JSON.stringify(battery);
-        },
-      })
-    );
+    anime({
+      targets: battery,
+      charged: "100%",
+      cycles: 130,
+      round: 1,
+      loop: true,
+      update: function () {
+        document.querySelector(".battery-log").innerHTML =
+          JSON.stringify(battery);
+      },
+    });
 
     // changing css properties
-    animation.current.add(
-      anime({
-        targets: ".box-3",
-        translateX: 240,
-        loop: true,
-        backgroundColor: "#fff",
-        borderRadius: ["0%", "50%"],
-        easing: "easeInOutQuad",
-      })
-    );
+    anime({
+      targets: ".box-3",
+      translateX: 240,
+      loop: true,
+      backgroundColor: "#fff",
+      borderRadius: ["0%", "50%"],
+      easing: "easeInOutQuad",
+    });
 
     // changing css transform properties
-    animation.current.add(
-      anime({
-        targets: ".box-4",
-        translateX: 250,
-        loop: true,
-        scale: 2,
-        rotate: "1turn",
-      })
-    );
+    anime({
+      targets: ".box-4",
+      translateX: 250,
+      loop: true,
+      scale: 2,
+      rotate: "1turn",
+    });
 
     // changing svg attributes
-    animation.current.add(
-      anime({
-        targets: "#svg-1 polygon",
-        points: "64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96",
-        baseFrequency: 0,
-        scale: 1,
-        direction: "alternate",
-        loop: true,
-        easing: "easeInOutExpo",
-      })
-    );
+    anime({
+      targets: "#svg-1 polygon",
+      points: "64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96",
+      baseFrequency: 0,
+      scale: 1,
+      direction: "alternate",
+      loop: true,
+      easing: "easeInOutExpo",
+    });
 
     // specific property parameters
-    animation.current.add(
-      anime({
-        targets: ".box-5",
-        translateX: {
-          value: 250,
-          duration: 800,
-        },
-        rotate: {
-          value: 360,
-          duration: 1800,
-          easing: "easeInOutSine",
-        },
-        scale: {
-          value: 2,
-          duration: 1600,
-          delay: 800,
-          easing: "easeInOutQuart",
-        },
-        delay: 250, // all property except 'scale' inherit 250ms delay
-        loop: true,
-      })
-    );
+    anime({
+      targets: ".box-5",
+      translateX: {
+        value: 250,
+        duration: 800,
+      },
+      rotate: {
+        value: 360,
+        duration: 1800,
+        easing: "easeInOutSine",
+      },
+      scale: {
+        value: 2,
+        duration: 1600,
+        delay: 800,
+        easing: "easeInOutQuart",
+      },
+      delay: 250, // all property except 'scale' inherit 250ms delay
+      loop: true,
+    });
 
     // funtion based parameters
-    animation.current.add(
-      anime({
-        targets: ".box-6",
-        translateX: 270,
-        direction: "alternate",
-        loop: true,
-        delay: function (el, i, l) {
-          return i * 100;
-        },
-        endDelay: function (el, i, l) {
-          return (l - i) * 100;
-        },
-      })
-    );
+    anime({
+      targets: ".box-6",
+      translateX: 270,
+      direction: "alternate",
+      loop: true,
+      delay: function (el, i, l) {
+        return i * 100;
+      },
+      endDelay: function (el, i, l) {
+        return (l - i) * 100;
+      },
+    });
 
     // function based values
-    animation.current.add(
-      anime({
-        targets: ".box-7",
-        translateX: function (el) {
-          return el.getAttribute("data-x");
-        },
-        translateY: function (el, i) {
-          return 100 + -90 * i;
-        },
-        scale: function (el, i, l) {
-          return l - i + 0.25;
-        },
-        rotate: function () {
-          return anime.random(-360, 360);
-        },
-        borderRadius: function () {
-          return ["50%", anime.random(10, 35) + "%"];
-        },
-        duration: function () {
-          return anime.random(1600, 2200);
-        },
-        delay: function () {
-          return anime.random(0, 500);
-        },
-        direction: "alternate",
-        loop: true,
-      })
-    );
+    anime({
+      targets: ".box-7",
+      translateX: function (el) {
+        return el.getAttribute("data-x");
+      },
+      translateY: function (el, i) {
+        return 100 + -90 * i;
+      },
+      scale: function (el, i, l) {
+        return l - i + 0.25;
+      },
+      rotate: function () {
+        return anime.random(-360, 360);
+      },
+      borderRadius: function () {
+        return ["50%", anime.random(10, 35) + "%"];
+      },
+      duration: function () {
+        return anime.random(1600, 2200);
+      },
+      delay: function () {
+        return anime.random(0, 500);
+      },
+      direction: "alternate",
+      loop: true,
+    });
 
     // property keyframes
     anime({
@@ -179,6 +162,57 @@ const App = () => {
         { value: 1, duration: 450 },
       ],
       easing: "easeOutElastic(1, 0.8)",
+      loop: true,
+    });
+
+    // Staggering animations
+    // staggering basics
+    anime({
+      targets: ".box-9",
+      translateX: 270,
+      delay: anime.stagger(100),
+      direction: "alternate",
+      loop: true,
+    });
+    // range value
+    anime({
+      targets: ".box-10",
+      translateX: 270,
+      rotate: anime.stagger([-360, 360]),
+      easing: "easeInOutQuad",
+      direction: "alternate",
+      loop: true,
+    });
+    // grid stagger
+    anime({
+      targets: ".box-11",
+      scale: [
+        { value: 0.1, easeing: "easeOutSine", duration: 500 },
+        { value: 1, easeing: "easeInOutQuad", duration: 1200 },
+      ],
+      delay: anime.stagger(200, { grid: [14, 5], from: "center" }),
+      loop: true,
+    });
+    // grid axis
+    anime({
+      targets: ".box-12",
+      translateX: anime.stagger(10, {
+        grid: [14, 5],
+        from: "center",
+        axis: "x",
+      }),
+      translateY: anime.stagger(10, {
+        grid: [14, 5],
+        from: "center",
+        axis: "y",
+      }),
+      rotateZ: anime.stagger([0, 90], {
+        grid: [14, 5],
+        from: "center",
+        axis: "x",
+      }),
+      delay: anime.stagger(200, { grid: [14, 5], from: "center" }),
+      direction: "alternate",
       loop: true,
     });
   }, []);
@@ -230,6 +264,28 @@ const App = () => {
       </div>
       <div className="wrap">
         <Box cs={"circle box-8 bg-bright-green"} />
+      </div>
+      <div className="wrap">
+        <Box cs={"small-box box-9 bg-green-an"} />
+        <Box cs={"small-box box-9 bg-green-an"} />
+        <Box cs={"small-box box-9 bg-green-an"} />
+        <Box cs={"small-box box-9 bg-green-an"} />
+        <Box cs={"small-box box-9 bg-green-an"} />
+        <Box cs={"small-box box-9 bg-green-an"} />
+      </div>
+      <div className="wrap">
+        <Box cs={"small-box box-10 bg-green-an"} />
+        <Box cs={"small-box box-10 bg-green-an"} />
+        <Box cs={"small-box box-10 bg-green-an"} />
+        <Box cs={"small-box box-10 bg-green-an"} />
+        <Box cs={"small-box box-10 bg-green-an"} />
+        <Box cs={"small-box box-10 bg-green-an"} />
+      </div>
+      <div className="wrap">
+        <Matrix cs={"small-box box-11 bg-green-an"} />
+      </div>
+      <div className="wrap">
+        <Matrix cs={"small-box box-12 bg-green-an"} />
       </div>
     </>
   );
---
+++ src/components/Matrix.jsx
@@ -0,0 +1,90 @@
+import Box from "./Box";
+
+const Matrix = ({ cs }) => {
+  return (
+    <>
+      <div className="row">
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+      </div>
+      <div className="row">
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+      </div>
+      <div className="row">
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+      </div>
+      <div className="row">
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+      </div>
+      <div className="row">
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+        <Box cs={cs} />
+      </div>
+    </>
+  );
+};
+
+export default Matrix;
