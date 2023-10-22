c658b704eb95cf846c075a7244a3a1814ae2adee
Himanshu <himanshu_196018@saitm.org>
2023-10-22 21:42:08 +0530

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -281,6 +281,9 @@ const App = () => {
     controlsEl.addEventListener("input", function () {
       tlc.current.seek(tlc.current.duration * (controlsEl.value / 100));
     });
+
+    // SVG animations
+    // Motion Path
     const path = anime.path(".box-15-svg path");
     anime({
       targets: ".box-15",
@@ -290,6 +293,56 @@ const App = () => {
       easing: "linear",
       duration: 2000,
       loop: true,
+    });
+
+    // Morphing
+    anime({
+      targets: ".box-16.polymorph",
+      points: [
+        {
+          value: [
+            "70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369",
+            "70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369",
+          ],
+        },
+        {
+          value:
+            "70 6 119.574 60.369 100.145 117.631 39.855 117.631 55.426 68.369",
+        },
+        {
+          value:
+            "70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369",
+        },
+        {
+          value:
+            "70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369",
+        },
+      ],
+      easing: "easeOutQuad",
+      duration: 2000,
+      loop: true,
+    });
+
+    // line drawing
+    anime({
+      targets: "#svg-2 .lines path",
+      strokeDashoffset: [anime.setDashoffset, 0],
+      easing: "easeInOutSine",
+      duration: 1500,
+      delay: function (el, i) {
+        return i * 250;
+      },
+      direction: "alternate",
+      loop: true,
+    });
+
+    // easing: step
+    anime({
+      targets: ".box-17",
+      translateX: 280,
+      loop: true,
+      easing: "steps(5)",
+      direction: "alternate",
     });
   }, []);
 
@@ -396,6 +449,9 @@ const App = () => {
           />
         </div>
       </div>
+      <div className="wrap">
+        <Box cs={"box box-17 bg-blue"} />
+      </div>
       <div className="wrap clr-violet wrap-box-15">
         <Box cs={"small-box bg-violet box-15"} />
         <svg
@@ -410,6 +466,71 @@ const App = () => {
             strokeWidth="1"
             d="M8,56 C8,33.90861 25.90861,16 48,16 C70.09139,16 88,33.90861 88,56 C88,78.09139 105.90861,92 128,92 C150.09139,92 160,72 160,56 C160,40 148,24 128,24 C108,24 96,40 96,56 C96,72 105.90861,92 128,92 C154,93 168,78 168,56 C168,33.90861 185.90861,16 208,16 C230.09139,16 248,33.90861 248,56 C248,78.09139 230.09139,96 208,96 L48,96 C25.90861,96 8,78.09139 8,56 Z"
           ></path>
+        </svg>
+      </div>
+      <div className="wrap clr-violet">
+        <svg width="140" height="140" viewBox="0 0 140 140">
+          <g fill="none" fillRule="evenodd">
+            <g fill="currentColor" fillOpacity=".15" transform="translate(0 6)">
+              <polygon points="70 0 136.574 48.369 111.145 126.631 28.855 126.631 3.426 48.369"></polygon>
+              <polygon points="70 18 119.455 53.931 100.565 112.069 39.435 112.069 20.545 53.931"></polygon>
+              <polygon points="70 34.86 101.727 57.911 89.609 95.209 50.391 95.209 38.273 57.911"></polygon>
+              <polygon points="70 50.898 84.864 61.697 79.186 79.171 60.814 79.171 55.136 61.697"></polygon>
+            </g>
+            <polygon
+              className="box-16 polymorph"
+              strokeWidth="1"
+              stroke="currentColor"
+              points="70 36.46349066694431 125.99458610115313 58.10291078782831 95.99050311101855 111.21041389884687 42.54600622203713 113.33912759622041 16.644853737668207 58.14581535361948 "
+            ></polygon>
+          </g>
+        </svg>
+      </div>
+      <div className="wrap clr-violet">
+        <svg id="svg-2" viewBox="0 0 280 100">
+          <g
+            fill="none"
+            fillRule="evenodd"
+            stroke="currentColor"
+            strokeWidth="1"
+            className="lines"
+          >
+            <path
+              className="el"
+              d="M58 80V50.12C57.7 41.6 51.14 35 43 35a15 15 0 0 0 0 30h7.5v15H43a30 30 0 1 1 0-60c16.42 0 29.5 13.23 30 29.89V80H58z"
+              strokeDasharray="316.85528564453125"
+            ></path>
+            <path
+              className="el"
+              d="M73 80V20H58v60h15z"
+              strokeDasharray="150"
+            ></path>
+            <path
+              className="el"
+              d="M58 80V49.77C58.5 33.23 71.58 20 88 20a30 30 0 0 1 30 30v30h-15V50a15 15 0 0 0-15-15c-8.14 0-14.7 6.6-15 15.12V80H58zm75 0V20h-15v60h15z"
+              strokeDasharray="441.1739501953125"
+            ></path>
+            <path
+              className="el"
+              d="M118 80V49.77C118.5 33.23 131.58 20 148 20a30 30 0 0 1 30 30v30h-15V50a15 15 0 0 0-15-15c-8.14 0-14.7 6.6-15 15.12V80h-15zm-7.5-60a7.5 7.5 0 1 1-7.48 8v-1c.25-3.9 3.5-7 7.48-7z"
+              strokeDasharray="338.3053894042969"
+            ></path>
+            <path
+              className="el"
+              d="M133 65a15 15 0 0 1-15-15v-7.5h-15V50a30 30 0 0 0 30 30V65zm30 15V49.77C163.5 33.23 176.58 20 193 20a30 30 0 0 1 30 30v30h-15V50a15 15 0 0 0-15-15c-8.14 0-14.7 6.6-15 15.12V80h-15z"
+              strokeDasharray="406.8699035644531"
+            ></path>
+            <path
+              className="el"
+              d="M238 65a15 15 0 0 1 0-30c8.1 0 14.63 6.53 15 15h-15v15h30V49.89C267.5 33.23 254.42 20 238 20a30 30 0 0 0 0 60V65z"
+              strokeDasharray="301.8561706542969"
+            ></path>
+            <path
+              className="el"
+              d="M260.48 65a7.5 7.5 0 1 1-7.48 8v-1c.26-3.9 3.5-7 7.48-7z"
+              strokeDasharray="47.128875732421875"
+            ></path>
+          </g>
         </svg>
       </div>
     </>
